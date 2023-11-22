313b11f468359d617be422bc7b144b592bad1a7f
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-22 17:38:51 +0900

    진짜끝이다이제..

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -29,8 +29,8 @@ const Header = () => {
       </HeaderDivider>
       <HeaderDivider>
         {!userData ? (
-          //"https://auth.bssm.kro.kr/oauth?clientId=d674a77d&redirectURI=https://onegane.kro.kr/redirect"
-          <a href="https://auth.bssm.kro.kr/oauth?clientId=d674a77d&redirectURI=http://localhost:3000/redirect">
+          //https://auth.bssm.kro.kr/oauth?clientId=d674a77d&redirectURI=http://localhost:3000/redirect
+          <a href="https://auth.bssm.kro.kr/oauth?clientId=d674a77d&redirectURI=https://onegane.kro.kr/redirect">
             <RegisterBtn>로그인</RegisterBtn>
           </a>
         ) : (
